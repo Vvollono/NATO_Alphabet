2eb6b541df59e4e59e8075e6b2fffe58d0c0fa76
Vincenzo Vollono <vvollono025@gmail.com>
2021-10-28 10:28:34 +0100

    Update nato_alphabet.py

--- nato_alphabet.py
+++ nato_alphabet.py
@@ -1,7 +1,9 @@
 import pandas
 
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
+# Create a dictonary from the dataframe using dictonary comprehension
 alph = {x["letter"] : x["code"] for x in data.to_dict(orient="records")}
 name = input("What is the name? ").upper()
+#Create a list from the dictonary using list comprehension
 list_name = [alph[letter]for letter in name]
 print(list_name)
